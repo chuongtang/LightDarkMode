fb4d8a82ae6de77d7f74c4a383ec6a6aa5ec95a5
Chuong Tang <chuong.tang@aol.com>
2021-03-17 11:00:33 -0700

    Fix icon toggle
    Fix Sun/ moon icon toggle

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
+const sunMoon = toggleIcon.querySelector('i');
 const textBox = document.getElementById('text-box');
 let navColor = document.getElementById('nav').style.backgroundColor;
 let textBoxColor = textBox.style.backgroundColor;
 const lightColor = 'rgb(255 255 255 / 50%)';
 const darkColor = 'rgb(0 0 0 / 50%)';
 let modeIcon = toggleIcon.children[1].classList;
-
 
 // Function mode selection
 function modeSelection(color){
@@ -18,19 +18,26 @@ function modeSelection(color){
     for (let i=1; i<5; i++) {
         document.getElementById('image'+i).src=`img/${i}_${color}.svg`;    
     }
-    (color === 'light')? textBoxColor = darkColor : textBoxColor = lightColor;  
-    (color === 'light')? modeIcon = "fas fa-sun" : modeIcon = "fas fa-moon" ; 
-}
+    // console.log(toggleIcon);
+    if (navColor === 'lightcolor'){
+        textBoxColor = lightColor;
+        sunMoon.classList = "fas fa-sun";
+    } else {
+        textBoxColor = darkColor;
+        sunMoon.classList = "fas fa-moon";
+    }
 
+    // (color === 'light')? textBoxColor = darkColor : textBoxColor = lightColor;  
+    // (color === 'dark')? modeIcon = "fas fa-moon" : modeIcon = "fas fa-sun" ; 
+}
 
 // Switch theme dynamically
 function switchTheme(event) {
-    console.log(event.target.checked);
+    // console.log(event.target.checked);
     (event.target.checked)? modeSelection('dark') : modeSelection('light');
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
-
 
 //  Check local storage for theme
 const currentTheme = localStorage.getItem('theme');
